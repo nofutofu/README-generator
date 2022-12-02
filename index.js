18eb5b3eded7893b28eb6f28c125d905f5814ea0
nofutofu <aaronrweis@gmail.com>
2022-12-02 02:44:48 -0600

    first functionality of writetofile function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -41,7 +42,12 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) throw err;
+    constructTeam();
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
